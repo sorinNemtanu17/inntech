653463c6bc98fb5ad0a06593f8733a7099bdb8ac
sorinNemtanu17 <sorinlego@gmail.com>
2022-07-01 17:06:31 +0300

    Updates

---
+++ precache-manifest.08669a77346a5992a95d955540780356.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ff8635e0dc0495c2767",
+    "url": "/sorinNemtanu17/inntech/static/css/main.1e79baa9.chunk.css"
+  },
+  {
+    "revision": "7ff8635e0dc0495c2767",
+    "url": "/sorinNemtanu17/inntech/static/js/main.7ff8635e.chunk.js"
+  },
+  {
+    "revision": "d11720e887a1448ed361",
+    "url": "/sorinNemtanu17/inntech/static/js/1.d11720e8.chunk.js"
+  },
+  {
+    "revision": "2eb26073e642365ce63d",
+    "url": "/sorinNemtanu17/inntech/static/js/2.2eb26073.chunk.js"
+  },
+  {
+    "revision": "1bf07de2be96b20e07c1",
+    "url": "/sorinNemtanu17/inntech/static/js/runtime~main.1bf07de2.js"
+  },
+  {
+    "revision": "5d99f510f2b9b4e4d8f8bc752cf468ea",
+    "url": "/sorinNemtanu17/inntech/static/media/hourglass_empty_black_24dp.5d99f510.svg"
+  },
+  {
+    "revision": "a9b7301df493b29d35d9cbf9bf51538f",
+    "url": "/sorinNemtanu17/inntech/static/media/hourglass_empty_black_24dp(1).a9b7301d.svg"
+  },
+  {
+    "revision": "af0e560c2e0630029d4da199618dd41e",
+    "url": "/sorinNemtanu17/inntech/static/media/Group 1782.af0e560c.svg"
+  },
+  {
+    "revision": "57a62c1190c4063da7f066b2cadb6f1d",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 237.57a62c11.svg"
+  },
+  {
+    "revision": "de3ad99e1d83b8bfd6dc25376f6e018c",
+    "url": "/sorinNemtanu17/inntech/static/media/287927452(7).de3ad99e.svg"
+  },
+  {
+    "revision": "733d8cd156db1eb5cc2768f96bea09e7",
+    "url": "/sorinNemtanu17/inntech/static/media/287927452(10).733d8cd1.svg"
+  },
+  {
+    "revision": "87f3fef4ea0fd011c0313eb0e79613b5",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 251.87f3fef4.svg"
+  },
+  {
+    "revision": "3d28fe3392bad168c4e4c718911cf862",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 236.3d28fe33.svg"
+  },
+  {
+    "revision": "86b9a74a2304a8a5976be17af00b06e6",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 235.86b9a74a.svg"
+  },
+  {
+    "revision": "34f06c28231b7d9f57903d2f2f5ad9a2",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 224.34f06c28.svg"
+  },
+  {
+    "revision": "ccd98302960fdfbb3621d4ea8a50e59d",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 234.ccd98302.svg"
+  },
+  {
+    "revision": "657e1741696c266ef884e77ce50b6c17",
+    "url": "/sorinNemtanu17/inntech/static/media/Path 233.657e1741.svg"
+  },
+  {
+    "revision": "a5c5c05f6cf4d9e6b76a75c0c29924fc",
+    "url": "/sorinNemtanu17/inntech/static/media/Group 1782 copy.a5c5c05f.svg"
+  },
+  {
+    "revision": "dcc7ab08c9323efaeac23626b7dbda66",
+    "url": "/sorinNemtanu17/inntech/static/media/Facebook.dcc7ab08.svg"
+  },
+  {
+    "revision": "75bd06b3b1ba2b7cde79f84037e419d9",
+    "url": "/sorinNemtanu17/inntech/static/media/Instagram.75bd06b3.svg"
+  },
+  {
+    "revision": "b119d1cd298bc34b58617bd7d3af2895",
+    "url": "/sorinNemtanu17/inntech/static/media/linkedin.b119d1cd.svg"
+  },
+  {
+    "revision": "823155589547a5c622ff465a8c3e1cb0",
+    "url": "/sorinNemtanu17/inntech/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f80b46181317fe92224fb59c95d31e5.js"
+  "/sorinNemtanu17/inntech/precache-manifest.08669a77346a5992a95d955540780356.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sorinNemtanu17/inntech/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
